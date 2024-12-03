d0bf1f290985c60af0dfe177140b7e8982a65c66
Vado-hyh <vul4ul4cjo@gmail.com>
2024-12-03 19:31:46 +0800

    RWD edit

--- WeHelp_Application.js
+++ WeHelp_Application.js
@@ -1,17 +1,16 @@
 // JavaScript
 document.addEventListener("DOMContentLoaded", () => {
-    const toggleMenu = document.querySelector('.toggle'); // 選取 "Question List"
-    const questionMenu = document.querySelector('.question-menu'); // 選取包含下拉選單的父元素
+    const toggleMenu = document.querySelector('.toggle');
+    const questionMenu = document.querySelector('.question-menu');
 
     toggleMenu.addEventListener('click', (event) => {
-        event.preventDefault(); // 阻止默認的跳轉行為
-        questionMenu.classList.toggle('active'); // 切換 active class
+        event.preventDefault(); 
+        questionMenu.classList.toggle('active');
     });
 
-    // 選單以外區域點擊時關閉下拉選單
     document.addEventListener('click', (event) => {
         if (!questionMenu.contains(event.target)) {
-            questionMenu.classList.remove('active'); // 移除 active class
+            questionMenu.classList.remove('active');s
         }
     });
 });
